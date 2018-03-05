3057d5fe66f07a57049e8af4d620d1c2bfa6a95f
J. Daniel Espinosa Rodriguez <danielespinosa.bin@gmail.com>
2018-03-05 12:21:24 -0600

    send message

--- config/default.js
+++ config/default.js
@@ -2,6 +2,6 @@ var export_config = {}
 
 
   export_config.pageToken = process.env.PAGE_ACCESS_TOKEN ? process.env.PAGE_ACCESS_TOKEN : "";
-  export_config.verifyToken =   process.env.VERIFY_TOKEN ? process.env.VERIFY_TOKEN : "P0nt3r1k0s0!";
+  export_config.verifyToken =   process.env.VERIFY_TOKEN ? process.env.VERIFY_TOKEN : "";
 
   module.exports = export_config;
