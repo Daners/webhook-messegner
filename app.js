d05863cd9a89ad7f76d506bd63c0f3650d1cdd76
J. Daniel Espinosa Rodriguez <danielespinosa.bin@gmail.com>
2018-03-23 11:57:21 -0600

    quitlogs

--- app.js
+++ app.js
@@ -50,15 +50,15 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-      console.log(JSON.stringify(entry));
+    //  console.log(JSON.stringify(entry));
       // Gets the message. entry.messaging is an array, but
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
+      //console.log(webhook_event);
       // Get the sender PSID
       if(webhook_event.sender){
         let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
+      //  console.log('Sender PSID: ' + sender_psid);
         // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
         if (webhook_event.message) {
@@ -180,7 +180,7 @@ function handleMessage(sender_psid, received_message) {
                           }
                }
 
-               console.log(response);
+              // console.log(response);
 
                  callSendAPI(sender_psid, response);
 
