1fdf84fc310ebc831f05bd4b761e24b39c83958c
Daners <danielespinosa.bin@gmail.com>
2018-06-04 17:49:10 -0500

    telmex.com

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ function handleMessage(sender_psid, received_message) {
               "template_type":"open_graph",
               "elements":[
                  {
-                  "url":"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
+                  "url":"http://telmex.com/web/negocios",
                   "buttons":[
                     {
                       "type":"web_url",
