50f3c3ca864453107447f3ca91b86adc0b10d039
Daners <danielespinosa.bin@gmail.com>
2018-06-15 16:48:30 -0500

    send 200

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/webhook', (req, res) => {
     });
     // Returns a '200 OK' response to all requests
   //  res.status(200).send('EVENT_RECEIVED');
-    res.ok()
+    res.sendStatus(200);
   } else {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
