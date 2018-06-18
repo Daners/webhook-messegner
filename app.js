b3704a42e61cd200625104e64ed50a9262b36bd0
Daners <danielespinosa.bin@gmail.com>
2018-06-18 11:43:59 -0500

    log body

--- app.js
+++ app.js
@@ -245,7 +245,7 @@ function handleMessageWatson(sender_psid, received_message){
       if (!err) {
         let response =[];
           response = processor.postProccesMessage(body);
-          console.log(JSON.stringify(response));
+          console.log(JSON.stringify(body));
           for (var i = 0,res; res = response[i++];) {
              callSendAPI(sender_psid, res);
           }
