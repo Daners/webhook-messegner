4073595107fdbf993991842c1ff9e969752d90e0
Daners <danielespinosa.bin@gmail.com>
2018-06-18 10:59:31 -0500

    bull

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ app.use(function(req, res, next) {
 function handleMessageWatson(sender_psid, received_message){
 
 //  let context = getContext(sender_psid);
-  let payload = processor.preProccesMessage(sender_psid,received_message,context);
+  let payload = processor.preProccesMessage(sender_psid,received_message,null);
   // if(!context){
   //     payload.input.text = "";
   // }
