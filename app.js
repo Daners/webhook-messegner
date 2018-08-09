4b554485097edece0a70c819d6bef6e05ef857a9
Daniel Espinosa <danielespinosa.bin@gmail.com>
2018-08-09 17:13:50 -0500

    push heroku

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.post('/webhook', (req, res) => {
 
 
         if(webhook_event.message && webhook_event.message.is_echo ){
+          sender_psid = webhook_event.recipient.id;
           console.log("SEND REPKY....."+sender_psid);
           var   response = { "text": "Hola Yo te seguire atendiendo aplicacion 2" }
           callSendAPI(sender_psid,response)
